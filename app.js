3629794a8f0512b4c766899f2865aad0dccfb69d
Imdni8 <tousif.rahaman@gmail.com>
2023-09-10 18:37:40 +0530

    Auto reloading for some reason

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let time = 19
+let time = 3 //make this 19
 
 //after one minute
-setInterval(settingTime, 1000*2)
+const myInterval = setInterval(settingTime, 1000*2) //make it 1000*60
 
 function substractor() {
     time = time - 1
 }
 
-//substracting one minute in display
+//substracting one minute in display -- main logic
 function settingTime() {
     if (time != 0) {
         document.querySelector("#countdown").innerHTML = time
@@ -16,11 +16,16 @@ function settingTime() {
     } 
     
     else {
-        document.querySelector("#countdown").innerHTML = "Please blink your eyes, look away from the screen and focus 20 meters away for 20 seconds."
-    }
-    
-}
+        clearInterval(myInterval)
 
-// function resetClock() {
-//     document.querySelector("#countdown").innerHTML = 20
-// }
+        document.querySelector("#countdown").innerHTML = "Please blink your eyes, look away from the screen and focus 20 meters away for 20 seconds."
+        document.querySelector('p').remove()
+        document.querySelector("h3").remove()
+        
+        //creating the button for resetting
+        const resetBtn = document.createElement("button")
+        resetBtn.innerHTML = "Reset timer"
+        document.body.appendChild(resetBtn)
+        resetBtn.addEventListener("click", console.log("reset btn clicked"))
+    }
+}

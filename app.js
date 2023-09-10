c7a40343b5251a52fd2f43059ecf20970086ae4f
Imdni8 <tousif.rahaman@gmail.com>
2023-09-10 15:08:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let time = 19
 
 //after one minute
-setInterval(settingTime, 1000*5)
+setInterval(settingTime, 1000*2)
 
 function substractor() {
     time = time - 1
@@ -9,7 +9,18 @@ function substractor() {
 
 //substracting one minute in display
 function settingTime() {
-    document.querySelector("#countdown").innerHTML = time
-    console.log(`time changed ${time}th time`)
-    substractor()
+    if (time != 0) {
+        document.querySelector("#countdown").innerHTML = time
+        console.log(`time changed`)
+        substractor()
+    } 
+    
+    else {
+        document.querySelector("#countdown").innerHTML = "Please blink your eyes, look away from the screen and focus 20 meters away for 20 seconds."
+    }
+    
 }
+
+// function resetClock() {
+//     document.querySelector("#countdown").innerHTML = 20
+// }
